34e2848df8291d7fc24c267ec586e11536386478
zCoCo <connor.caxd@gmail.com>
2019-09-21 18:35:20 -0400

    Torsional Test 1

--- Lab 1/DataTable.m
+++ Lab 1/DataTable.m
@@ -1,7 +1,9 @@
 % Creates an ETable from the Raw Data File
 function T = DataTable()
     % Load Every Column, Giving them Each a Variable Name:
-    T = ETable('test1.xlsx', ["t","F","x1", "x2","v1", "v2","a1", "a2"]); % Long-form names used in plotting are automatically imported from column headers
+    %T = ETable('test1.xlsx', ["t","F","x1", "x2","v1", "v2","a1", "a2"]); % Long-form names used in plotting are automatically imported from column headers
+    %save('test1.mat', 'T'); % Cached loaded file
+    load('test1.mat', 'T'); % Load cached file
     % Assign (latex formatted) Units to Each Column:
     T.unitsList = ["s","N", "m","m", "$\tfrac{m}{s}$","$\tfrac{m}{s}$", "$\tfrac{m}{s^2}$","$\tfrac{m}{s^2}$"];
     
@@ -11,7 +13,8 @@ function T = DataTable()
     
     % Basic Exploratory Plots:
     figure();
-    hold on
-        T.plot('t', 'F');
-        T.plot('t' 
+    T.multiplot('t', 'F', 'x1','x2'); % Plot F, x1, and x2 vs t
+    
+    figure();
+    T.plot('x1','F');
 end
